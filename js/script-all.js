1fe66caa572267b00c87a115583aa5dbd695f21c
Joab Mendes <joab.mendes.r2d2@gmail.com>
2013-08-25 04:28:58 -0300

    update commit

--- js/script-all.js
+++ js/script-all.js
@@ -2,9 +2,17 @@
 
 
 
-function pushScreenModulo(sreenID, averageValue){
-	localStorage.setItem(sreenID, document.getElementById(averageValue).value);
-	bb.pushScreen(sreenID+'.html', sreenID+'Screen');
+function pushScreenForm(minmedia, qtdnotas){
+	var minmedia_value = document.getElementById(minmedia).value
+	var qtdnotas_value = document.getElementById(qtdnotas).value
+	if(minmedia_value && qtdnotas_value){
+		localStorage.setItem('minmedia', minmedia_value);
+		localStorage.setItem('qtdnotas', qtdnotas_value);
+		bb.pushScreen('displayform.html', 'displayform');
+	}else{
+		blackberry.ui.dialog.standardAskAsync("Insira as informa&ccedil;&otilde;es por favor", blackberry.ui.dialog.D_OK, null, {title : "Aten&ccedil;&atilde;o"});
+	}
+	
 }
 
 
