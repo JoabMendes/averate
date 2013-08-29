c1831feb17afbc02d0d064478b0b023c9c49d183
Joab Mendes <joab.mendes.r2d2@gmail.com>
2013-08-29 03:28:03 -0300

    update commit

--- js/script-all.js
+++ js/script-all.js
@@ -3,42 +3,85 @@
 
 
 function pushScreenForm(minmedia, qtdnotas){
-	var minmedia_value = document.getElementById(minmedia).value
-	var qtdnotas_value = document.getElementById(qtdnotas).value
-	if(minmedia_value > 0 && qtdnotas_value > 0 && qtdnotas_value < 9){
+	var minmedia_value = document.getElementById(minmedia).value;
+	var qtdnotas_value = document.getElementById(qtdnotas).value;
+	if(minmedia_value > 0 && qtdnotas_value > 1 && qtdnotas_value < 9){
 		localStorage.setItem('minmedia', minmedia_value);
 		localStorage.setItem('qtdnotas', qtdnotas_value);
 		bb.pushScreen('displayform.html', 'displayform');
 	}else{
-		if(qtdnotas_value > 8 && minmedia_value < 0){
+		if(minmedia_value === '' || qtdnotas_value === '' ){
+			blackberry.ui.dialog.standardAskAsync("Insira todas as informa&ccedil;&otilde;es por favor.", blackberry.ui.dialog.D_OK, null, {title : "Aten&ccedil;&atilde;o"});
+		}else if(qtdnotas_value > 8 && minmedia_value <= 0){
 			blackberry.ui.dialog.standardAskAsync("- Insira uma m&eacute;dia positiva <br/>- O limite m&aacute;ximo de notas &eacute; 8.", blackberry.ui.dialog.D_OK, null, {title : "Aten&ccedil;&atilde;o"});
 		}else if(qtdnotas_value > 8){
 			blackberry.ui.dialog.standardAskAsync("O limite m&aacute;ximo de notas &eacute; 8.", blackberry.ui.dialog.D_OK, null, {title : "Aten&ccedil;&atilde;o"});
-		}else if(minmedia_value < 0){
+		}else if(qtdnotas_value < 2){
+			blackberry.ui.dialog.standardAskAsync("O limite m&iacute;nimo de notas &eacute; 2.", blackberry.ui.dialog.D_OK, null, {title : "Aten&ccedil;&atilde;o"});
+		}else if(minmedia_value <= 0){
 			blackberry.ui.dialog.standardAskAsync("Insira uma m&eacute;dia positiva.", blackberry.ui.dialog.D_OK, null, {title : "Aten&ccedil;&atilde;o"});
-		}else{
-			blackberry.ui.dialog.standardAskAsync("Insira as informa&ccedil;&otilde;es por favor.", blackberry.ui.dialog.D_OK, null, {title : "Aten&ccedil;&atilde;o"});
 		}
 		
 	}
 	
 }
 
-/*
-*	Fazer todas alterações de localstorage para adiconar ou retirar input
-*	Mudar texto do label
-*	icrementar local storage do ídice
-*	chamar remove e colocar para o último indice
-*	
-*/
+
 
 function editForm(action){
-	if(action === '+'){
+
+	if(action === '+' && parseInt(localStorage.getItem('indexQtdNotas')) < 9){
 		$('a.add').trigger('click');
-	}else{
-		//onclick="$(this).parent().slideUp(function(){ $(this).remove();  this elemento do indice
-		//ver http://www.andresvidal.com/labs/relcopy.html
+		localStorage.setItem('indexQtdNotas', parseInt(localStorage.getItem('indexQtdNotas'))+1);
+		localStorage.setItem('qtdnotas', localStorage.getItem('indexQtdNotas'));
+		document.getElementById('label-nota'+parseInt(localStorage.getItem('indexQtdNotas'))).innerHTML = "Nota "+parseInt(localStorage.getItem('indexQtdNotas'));
+
+	}else if(action === '-' && parseInt(localStorage.getItem('indexQtdNotas')) > 2){
+		var idrmv = '#rownota'+localStorage.getItem('indexQtdNotas')+' a';
+		$(idrmv).trigger('click');
+		localStorage.setItem('indexQtdNotas', parseInt(localStorage.getItem('indexQtdNotas'))-1);
+		localStorage.setItem('qtdnotas', localStorage.getItem('indexQtdNotas'));
+	}
+
+	if(action === '+' && parseInt(localStorage.getItem('indexQtdNotas')) == 8){
+		blackberry.ui.dialog.standardAskAsync("O limite m&aacute;ximo de notas &eacute; 8.", blackberry.ui.dialog.D_OK, null, {title : "Aten&ccedil;&atilde;o"});
+	}
+	if(action === '-' && parseInt(localStorage.getItem('indexQtdNotas')) == 2){
+		blackberry.ui.dialog.standardAskAsync("O limite m&iacute;nimo de notas &eacute; 2.", blackberry.ui.dialog.D_OK, null, {title : "Aten&ccedil;&atilde;o"});
 	}
 }
 
 
+function pushScreenResult(){
+	var qtdnotas = localStorage.getItem('indexQtdNotas');
+	var i = 1;
+	var allnotas = new Array();
+	var allpesos = new Array();
+	allnotas[0] = document.getElementById('nota').value;
+	allpesos[0] = document.getElementById('peso').value;
+	while(i < qtdnotas){
+		allnotas[i] = document.getElementById('nota'+(i+1)).value;
+		allpesos[i] = document.getElementById('peso'+(i+1)).value;
+		i++;
+	}
+
+	if(isEmpty(allnotas) || isEmpty(allpesos)){
+		//Criar algoritmo que completa tela com todos as notas calculo de média e situação
+	}else{
+		//Criar algoritmo que considera as notas faltando e o valor das notas necessárias 
+		//calculo de média parcial e situação
+		 
+	}
+	//bb.pushScreen('displayresul.html', 'displayresul');
+}
+
+function isEmpty(array){
+	var i = 0;
+	while(i < array.length){
+		if(array[i] === ""){
+			return true;
+		}
+		i++;
+	}
+	return false;
+}
